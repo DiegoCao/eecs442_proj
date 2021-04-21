ab81c2c1cb9c1420fed25c4d352b603c23ba2e2a
Qiu Yuqing <qyuqing@umich.edu>
2021-04-21 19:04:03 +0800

    add lab2rgb

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import torch 
 import torchvision
-
+from torchsummary import summary
 import torch.nn as nn
 # create an abtract class base model
 class Encoder(nn.Module):
@@ -27,15 +27,15 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.Norm = norm
         self.upsample = nn.ConvTranspose2d(in_channels, out_channels, kernel_size= kernel, stride = stride, padding = 1)
-        self.conv = nn.Conv2d(cat_channels, out_channels, kernel_size = 4, stride = 1, padding=1)
+        self.conv = nn.Conv2d(cat_channels, out_channels, kernel_size = 3, stride = 1, padding=1)
         self.batchnorm = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
     
     def forward(self, x1, x2):
         x1 = self.upsample(x1)
         '''need to fix dim'''
-        x1 = torch.cat((x1, x2), dim = 3)
-        x1 = self.cov(x1)
+        x1 = torch.cat((x1, x2), dim = 1)
+        x1 = self.conv(x1)
         if self.Norm:
             x1 = self.batchnorm(x1)
         x1 = self.relu(x1)
@@ -46,50 +46,75 @@ class Decoder(nn.Module):
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
-        self.encode_layers = []
-        # THE STRIDE IS 1 here! No BatchNorm in first layer
-        self.encode_layers.append(Encoder(1, 64, 4, 1, 0)) # [32, 32, 1] -> [32, 32, 64]
-        self.encode_layers.append(Encoder(64, 128, 4, 2, 1)) # [32, 32, 64] -> [16, 16, 128]
-        self.encode_layers.append(Encoder(128, 256, 4, 2, 1)) # [16, 16, 128] -> [8, 8, 256]
-        self.encode_layers.append(Encoder(256, 512, 4, 2, 1)) # [8, 8, 256] -> [4, 4, 512]
-        self.encode_layers.append(Encoder(512, 512, 4, 2, 1)) # [4, 4, 512] -> [2, 2, 512]
+        # self.encode_layers = []
+        # # THE STRIDE IS 1 here! No BatchNorm in first layer
+        # self.encode_layers.append(Encoder(1, 64, 3, 1, 1, False)) # [32, 32, 1] -> [32, 32, 64]
+        # self.encode_layers.append(Encoder(64, 128, 4, 2, 1)) # [32, 32, 64] -> [16, 16, 128]
+        # self.encode_layers.append(Encoder(128, 256, 4, 2, 1)) # [16, 16, 128] -> [8, 8, 256]
+        # self.encode_layers.append(Encoder(256, 512, 4, 2, 1)) # [8, 8, 256] -> [4, 4, 512]
+        # self.encode_layers.append(Encoder(512, 512, 4, 2, 1)) # [4, 4, 512] -> [2, 2, 512]
 
         # self.decode_layers = []
         # self.decode_layers.append(Decoder(512, 512+512, 512, 4, 2, 1)) # [2, 2, 512] -> [4, 4, 512] -> [4, 4, 512]
         # self.decode_layers.append(Decoder(512, 256+256, 256, 4, 2, 1)) #[4, 4, 512] -> [8, 8, 256] ->[8, 8, 256]
         # self.decode_layers.append(Decoder(256, 128+128, 128, 4, 2, 1)) # [8, 8, 256] -> [16, 16, 128] -> [16, 16, 128]
         # self.decode_layers.append(Decoder(128, 64+64, 64, 4, 2, 1)) #[16, 16, 128] -> [32, 32, 64] -> [32, 32, 64]
+
+
+        # THE STRIDE IS 1 here! No BatchNorm in first layer
+        self.encode_layer1 = Encoder(1, 64, 3, 1, 1, False) # [32, 32, 1] -> [32, 32, 64]
+        self.encode_layer2 = Encoder(64, 128, 4, 2, 1) # [32, 32, 64] -> [16, 16, 128]
+        self.encode_layer3 = Encoder(128, 256, 4, 2, 1) # [16, 16, 128] -> [8, 8, 256]
+        self.encode_layer4 = Encoder(256, 512, 4, 2, 1) # [8, 8, 256] -> [4, 4, 512]
+        self.encode_layer5 = Encoder(512, 512, 4, 2, 1) # [4, 4, 512] -> [2, 2, 512]
+
+        self.decode_layer1 = Decoder(512, 512+512, 512, 4, 2, 1) # [2, 2, 512] -> [4, 4, 512] -> [4, 4, 512]
+        self.decode_layer2 = Decoder(512, 256+256, 256, 4, 2, 1) #[4, 4, 512] -> [8, 8, 256] ->[8, 8, 256]
+        self.decode_layer3 = Decoder(256, 128+128, 128, 4, 2, 1) # [8, 8, 256] -> [16, 16, 128] -> [16, 16, 128]
+        self.decode_layer4 = Decoder(128, 64+64, 64, 4, 2, 1) #[16, 16, 128] -> [32, 32, 64] -> [32, 32, 64]
         
-        # # last layer is 1 times 1 conv without batchNorm and with tanh activation fucntion
-        # # [32, 32, 64] -> [32, 32, 2]
-        # self.last_layer = nn.Sequential (
-        #     nn.Conv2d(64, 2, kernel_size = 1), 
-        #     nn.Tanh()
-        # )
+        # last layer is 1 times 1 conv without batchNorm and with tanh activation fucntion
+        # [32, 32, 64] -> [32, 32, 2]
+        self.last_layer = nn.Sequential (
+            nn.Conv2d(64, 2, kernel_size = 1), 
+            nn.Tanh()
+        )
 
 
     def forward(self, input):
-        x1 = self.encode_layers[0](input)
-        x2 = self.encode_layers[1](x1)
-        x3 = self.encode_layers[2](x2)
-        x4 = self.encode_layers[3](x3)
-        out = self.encode_layers[4](x4)
+        # x1 = self.encode_layers[0](input)
+        # x2 = self.encode_layers[1](x1)
+        # x3 = self.encode_layers[2](x2)
+        # x4 = self.encode_layers[3](x3)
+        # f = self.encode_layers[4](x4)
 
         # d4 = self.decode_layers[0](f, x4)
         # d3 = self.decode_layers[1](d4, x3)
         # d2 = self.decode_layers[2](d3, x2)
         # d1 = self.decode_layers[3](d2, x1)
         # out = self.last_layer(d1)
+
+        x1 = self.encode_layer1(input)
+        x2 = self.encode_layer2(x1)
+        x3 = self.encode_layer3(x2)
+        x4 = self.encode_layer4(x3)
+        f = self.encode_layer5(x4)
+
+        d4 = self.decode_layer1(f, x4)
+        d3 = self.decode_layer2(d4, x3)
+        d2 = self.decode_layer3(d3, x2)
+        d1 = self.decode_layer4(d2, x1)
+        out = self.last_layer(d1)
         return out
         
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.layer0 = Encoder(2, 64, 4, 2, 0)  # [32, 32, 2] -> [16, 16, 64]  First layer has no BatchNorm
+        self.layer0 = Encoder(3, 64, 4, 2, 1, False)  # [32, 32, 2] -> [16, 16, 64]  First layer has no BatchNorm
         self.layer1 = Encoder(64, 128, 4, 2, 1) # [16, 16, 64] -> [8, 8, 128]
         self.layer2 = Encoder(128, 256, 4, 2, 1) # [8, 8, 128] -> [4, 4, 256]
-        self.layer3 = Encoder(256, 512, 4, 1, 1) # [4, 4, 256] -> [4, 4, 512]
+        self.layer3 = Encoder(256, 512, 3, 1, 1) # [4, 4, 256] -> [4, 4, 512]
         self.last_layer = nn.Sequential(
             nn.Conv2d(512, 1, kernel_size = 4 ), # stride = 1
             nn.Sigmoid()
@@ -104,3 +129,20 @@ class Discriminator(nn.Module):
         return x
 
 
+# def initialize_weights(model):
+#     # Initializes weights according to the DCGAN paper
+#     for m in model.modules():
+#         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+#             nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+
+# generator = Generator()
+# discriminator = Discriminator()
+# initialize_weights(generator)
+# initialize_weights(discriminator)
+# summary(generator, input_size = (1, 32, 32))
+# summary(discriminator, input_size = (2, 32, 32))
+# encoder = Encoder(1, 64, 4, 2, 1)
+# summary(encoder, input_size = (1, 32, 32))
+# decoder = Decoder(512, 256+256, 256, 4, 2, 1)
+# summary(decoder, input_size = [(512, 4, 4), (256, 8, 8)])
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import torch 
 import os 
 import torch.nn as nn
+import numpy as np
+import cv2
 
 import torchvision 
 def downloadImages(whichdata = 'cifar10', data_dir = './data'):
@@ -19,4 +21,23 @@ def downloadImages(whichdata = 'cifar10', data_dir = './data'):
     return images 
 
 
-def rgb2lab()
+def lab2rgb(imgs):
+    lab_img = np.transpose(imgs.numpy(), (0, 2, 3, 1))
+    rgb_img = np.zeros(lab_img.shape)
+    N = lab_img.shape[0]
+    # print(lab_img)
+    for i in range(N): 
+        img = lab_img[i]
+        mean = np.array([0.5])
+        img = img/2
+        img += mean
+        img *= 255
+        img[:, :, 2] -= 128
+        img[:, :, 1] -= 128
+        img[:, :, 0] /= 2.55
+        img = cv2.cvtColor(img, cv2.COLOR_LAB2RGB)
+        img = img.astype(np.uint8)
+        rgb_img[i] = img
+    tensor_rgb_image = torch.from_numpy(np.transpose(rgb_img, (0, 3, 1, 2)))
+    # print(tensor_rgb_image)
+    return tensor_rgb_image
